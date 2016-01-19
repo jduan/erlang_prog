607af5f10327a479b1ec7fae750d3b1f7719bcce
Jingjing Duan <jduan@twitter.com>
2016-01-18 20:05:36 -0800

    WIP

--- test.erl
+++ test.erl
@@ -1,12 +1,27 @@
 -module(test).
--export([safe/1]).
+-export([equals/2, myand/2, incr/1, decr/1, add/2, subtract/2, multiply/2]).
 
-% This is a comment.
-% Everything on a line after % is ignored.
+equals(A, A) ->
+    true;
+equals(A, B) ->
+    false.
 
-safe(X) ->
-    Y = case X of
-            one -> 12;
-            _   -> 196
-        end,
-    X + Y.
+myand(true, true) -> true;
+myand(true, false) -> false;
+myand(false, true) -> false;
+myand(false, false) -> false.
+
+incr(A) -> A + 1.
+decr(A) -> A - 1.
+
+add(A, 0) -> A;
+add(A, B) when B < 0 -> add(decr(A), incr(B));
+add(A, B) -> add(incr(A), decr(B)).
+
+subtract(A, 0) -> A;
+subtract(A, B) when B < 0 -> subtract(incr(A), incr(B));
+subtract(A, B) -> subtract(decr(A), decr(B)).
+
+multiply(A, 1) -> A;
+multiply(A, B) when B < 0 -> 0 - multiply(A, -B);
+multiply(A, B) -> add(A, multiply(A, decr(B))).
